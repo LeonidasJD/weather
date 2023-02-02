f8a72d2701e932c5211a0f4aaec70d97982bfbed
Milan Djordjevic <milandjordjevic633@gmail.com>
2023-02-02 10:41:19 +0100

    added search bar and type of temp

--- weather/src/app/app.module.ts
+++ weather/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { MainWeatherComponent } from './main-weather/main-weather.component';
+import { WeatherDetailsComponent } from './weather-details/weather-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    MainWeatherComponent,
+    WeatherDetailsComponent
   ],
   imports: [
     BrowserModule
