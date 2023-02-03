e5f85b4950a9ffa737224ac3f16f3755bd2134e4
Milan Djordjevic <milandjordjevic633@gmail.com>
2023-02-03 10:08:41 +0100

    added c in module

--- weather/src/app/app.module.ts
+++ weather/src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { MainWeatherComponent } from './main-weather/main-weather.component';
 import { WeatherDetailsComponent } from './weather-details/weather-details.component';
+import { CurrentTempComponent } from './main-weather/current-temp/current-temp/current-temp.component';
+import { WeekTempComponent } from './main-weather/week-temp/week-temp/week-temp.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
     MainWeatherComponent,
-    WeatherDetailsComponent
+    WeatherDetailsComponent,
+    CurrentTempComponent,
+    WeekTempComponent,
+
   ],
   imports: [
     BrowserModule
