3d24c811f471ee2fbd689da8eb2585e35131339c
LeonidasJD <milandjordjevic633@gmail.com>
2023-03-14 09:51:10 +0100

    added height

--- weather/src/app/shared/directives/increase-height.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IncreaseHeightDirective } from './increase-height.directive';
-
-describe('IncreaseHeightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new IncreaseHeightDirective();
-    expect(directive).toBeTruthy();
-  });
-});
